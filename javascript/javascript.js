9d38ce28bfe091a4570c06196900ef9b5fbd9852
Pedro Pires <33130521+PaperButt@users.noreply.github.com>
2018-02-27 19:36:16 +0000

    Updated the Project Page
    Added a little dump search thing kind of

---
+++ javascript/javascript.js
@@ -0,0 +1,4 @@
+function change_link() {
+  $("#dump_link").attr("href", "http://paperbutt.github.io/Programming-Dump/Programming%20Dump/HTML/" + $("#dump_input").val() + "/index.html");
+  $("#dump_link").html("http://paperbutt.github.io/Programming-Dump/Programming%20Dump/HTML/" + $("#dump_input").val() + "/index.html");
+}
